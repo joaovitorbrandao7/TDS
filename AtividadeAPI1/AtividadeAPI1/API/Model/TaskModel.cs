4fa1fa937fbc31ef4e2c689044b7ad9bf75c0198
João Vitor Macedo <joao.macedo@bizapp.com.br>
2023-08-29 09:58:26 -0300

    TPTE #02: Cadastro Produtos - Com banco

---
+++ AtividadeAPI1/AtividadeAPI1/API/Model/TaskModel.cs
@@ -0,0 +1,22 @@
+﻿namespace AtividadeAPI1.API.Model
+{
+    public class TaskModel
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set;
+
+        }
+
+        public TaskModel(int id, string nome, decimal preco, int quantidade) {
+        
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+    }
+}
---
+++ AtividadeAPI1/AtividadeAPI1/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AtividadeAPI1.API.Model;
+
+namespace AtividadeAPI1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TaskModel>? Tasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        
+            => options.UseSqlite("DataSource=tds.db;Cache=Shared");
+       
+        }
+    }
+
